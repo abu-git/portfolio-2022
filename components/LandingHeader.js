a8db5dc639afb0fc6a1d55382b58dd41729a3494
abu-git <abu.sammie@gmail.com>
2022-05-05 03:15:21 +0200

    more projects improvements

--- components/LandingHeader.js
+++ components/LandingHeader.js
@@ -2,8 +2,8 @@
 
 function LandingHeader() {
   return (
-    <div className='bg-black container mx-auto mt-22'>
-        <header className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-16'>
+    <div className='bg-black container mx-auto mt-16'>
+        <header className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-16 mx-5'>
             <div className='flex justify-center'>
               <div className="mt-8 ripple">
                 <img className="object-cover rounded-full" src="/sam.jpg" alt="Profile image"/>
@@ -12,7 +12,7 @@ function LandingHeader() {
             <div className='flex flex-col justify-center'>
               <h2 className="text-center font-thin lg:text-7xl text-3xl md:text-5xl text-purple-400 mt-6 md:mt-12 mb-1">Hi there,</h2> 
               <h3 className='text-center font-light text-2xl md:text-4xl lg:text-5xl text-cyan-600 mt-0 mb-2'>
-                welcome to <span className="font-light text-3xl md:text-4xl lg:text-6xl ">Sam's</span> portfolio.
+                welcome to my portfolio.
               </h3>
             </div>
             
