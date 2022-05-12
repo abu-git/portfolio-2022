6ea1952ec9b00c67fe2090ad5b0ed74231f3d115
abu-git <abu.sammie@gmail.com>
2022-05-13 01:44:11 +0200

    nifty wears started

--- components/Projects.js
+++ components/Projects.js
@@ -93,7 +93,8 @@ function Projects() {
             {/*</a>*/}
             
 
-            <a target="_blank" href="https://nifty-wears.herokuapp.com/" rel="noreferrer">
+            {/*<a target="_blank" href="https://nifty-wears.herokuapp.com/" rel="noreferrer">*/}
+            <Link href="/projects/niftywears">
                 <div className='cursor-pointer opacity-90 hover:opacity-100 mb-5 pb-5 overflow-hidden rounded-md shadow-md hover:shadow-lg hover:shadow-purple-500/50 text-cyan-500 hover:text-purple-500  shadow-cyan-500/50 mx-5'>
                     <Image src="/nifty.jpg" height={533} width={533} alt="nifty" objectcover="true" />
                     <h3 className='text-center text-2xl uppercase font-light px-6'>Clothing Ordering App with Login/Signup</h3>
@@ -110,8 +111,9 @@ function Projects() {
 
                         <svg className='w-7 h-7 fill-indigo-400' role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Heroku</title><path d="M20.61 0H3.39C2.189 0 1.23.96 1.23 2.16v19.681c0 1.198.959 2.159 2.16 2.159h17.22c1.2 0 2.159-.961 2.159-2.159V2.16C22.77.96 21.811 0 20.61 0zm.96 21.841c0 .539-.421.96-.96.96H3.39c-.54 0-.96-.421-.96-.96V2.16c0-.54.42-.961.96-.961h17.22c.539 0 .96.421.96.961v19.681zM6.63 20.399L9.33 18l-2.7-2.4v4.799zm9.72-9.719c-.479-.48-1.379-1.08-2.879-1.08-1.621 0-3.301.421-4.5.84V3.6h-2.4v10.38l1.68-.78s2.76-1.26 5.16-1.26c1.2 0 1.5.66 1.5 1.26v7.2h2.4v-7.2c.059-.179.059-1.501-.961-2.52zM13.17 7.5h2.4c1.08-1.26 1.62-2.521 1.8-3.9h-2.399c-.241 1.379-.841 2.64-1.801 3.9z"/></svg>                       
                     </div>
-                </div>        
-            </a>
+                </div>
+            </Link>        
+            {/*<</a>*/}
             
 
             <a target="_blank" href="https://tufcat-books.herokuapp.com/" rel="noreferrer">
